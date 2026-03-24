1a4ed84494b8d6da1b50717f1f9f94dd79e3a0b3
Jhonatha Cirilo Silva <jcirilo@motorola.com>
2026-03-24 12:45:19 -0400

    REFAC: add trused host from deploy link, new requirements and some folders to gitignore

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from app.routes.device import router as device_router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi import FastAPI
+import webbrowser
+import uvicorn
 
 app = FastAPI(title='Fast Bridge API', description='API para controle de dispositivos Android via ADB e uiautomator2', version='1.0.0')
 app.include_router(device_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'], # TODO: quando for pra produção, colocar somente a url do site
+    allow_origins=['https://fast-bridge-nine.vercel.app'], # TODO: quando for pra produção, colocar somente a url do site
     allow_credentials=True,
-    allow_methods=['*'],
+    allow_methods=['GET', 'POST'],
     allow_headers=['*'],
 )
+
+if __name__ == '__main__':
+    print("Access https://fast-bridge-nine.vercel.app to get started")
+    webbrowser.open('https://fast-bridge-nine.vercel.app')
+    uvicorn.run(app, port=8000)
+
